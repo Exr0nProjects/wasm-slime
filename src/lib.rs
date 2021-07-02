b63ff369c5e42800122fc1ecabf9c23c8678f85e
Exr0n <mail@exr0n.com>
2021-07-02 00:29:21 -0700

    anim using rust!!!! gnnn

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use game_loop::game_loop;
+
 use wasm_bindgen::prelude::*;
 use web_sys::{ console, window, Node };
 use wasm_bindgen::JsCast;
@@ -27,6 +29,46 @@ pub fn gameloop() {
 
 }
 
+struct Agent {
+    pos_x: f64,
+    pos_y: f64,
+    heading: f64,   // radians
+}
+
+struct Dish {
+    size_w: u32,
+    size_h: u32,
+    agents: Vec<Agent>,
+}
+impl Dish {
+    fn new(size_w: u32, size_h: u32) -> Dish {
+        Dish { size_w, size_h, agents: Vec::new() }
+    }
+    fn render(&self, updates: u32) {
+        // TODO: lets not get the canvas from scratch every time
+        let window = web_sys::window().unwrap();
+        let document = window.document().unwrap();
+        let canvas = document.get_element_by_id("slime-canvas").unwrap();
+        let canvas: web_sys::HtmlCanvasElement = canvas
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .map_err(|_| ())
+            .unwrap();
+        let ctx = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
+
+        ctx.clear_rect(0., 0., canvas.width() as f64, canvas.height() as f64);
+
+        ctx.set_fill_style(&JsValue::from_str("green"));
+        console::log_1(&JsValue::from_str(&format!("amazing: {}", updates % 50)));
+        let updates = (updates % 50) as f64;
+        ctx.fill_rect(self.size_w as f64/2. - updates/2., self.size_h as f64/2. - updates/2., updates, updates);
+    }
+}
+
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -44,9 +86,6 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
-
-
-    
     let [win_wid, win_hei] = {
         let x = document.document_element().unwrap();
         [x.client_width(), x.client_height()]
@@ -59,12 +98,12 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    let [width, height] = [canvas.width() as f64, canvas.height() as f64];
+    let [width, height] = [canvas.width(), canvas.height()];
      
     // TODO: handle window resizing
 
-    ctx.set_fill_style(&JsValue::from_str("green"));
-    ctx.fill_rect(width/2. - 50., height/2. - 50., 100., 100.);
+    //ctx.set_fill_style(&JsValue::from_str("green"));
+    //ctx.fill_rect(width/2. - 50., height/2. - 50., 100., 100.);
     //
     //if let Some(win) = window() {
     //    console::log_1(&JsValue::from_str("Got window!"));
@@ -80,15 +119,24 @@ pub fn main_js() -> Result<(), JsValue> {
     //}
 
 
-    let tick = Closure::wrap(Box::new(move || {
-            console::log_1(&JsValue::from_str(&format!("amazing: {}", 3)));
-        }) as Box<dyn FnMut()>);
+    //let tick = Closure::wrap(Box::new(move || {
+    //        console::log_1(&JsValue::from_str(&format!("amazing: {}", 3)));
+    //    }) as Box<dyn FnMut()>);
+    //
+    ////let tick = Closure::wrap(Box::new(gameloop));
+    //
+    //// when rust has a java moment
+    //let interval_id = window.set_interval_with_callback_and_timeout_and_arguments_0(
+    //    tick.as_ref().unchecked_ref(), (1000. / FRAMERATE) as i32); // https://docs.rs/wasm-bindgen/0.2.74/wasm_bindgen/closure/struct.Closure.html
+    
 
-    //let tick = Closure::wrap(Box::new(gameloop));
-
-    // when rust has a java moment
-    let interval_id = window.set_interval_with_callback_and_timeout_and_arguments_0(
-        tick.as_ref().unchecked_ref(), (1000. / FRAMERATE) as i32); // https://docs.rs/wasm-bindgen/0.2.74/wasm_bindgen/closure/struct.Closure.html
+    let sim = Dish::new(width, height);
+    game_loop(sim, 240, 0.1, |g| {
+        // update fn
+    }, |g| {
+        // render fn
+        g.game.render(g.number_of_updates());
+    });
 
     Ok(())
 }
